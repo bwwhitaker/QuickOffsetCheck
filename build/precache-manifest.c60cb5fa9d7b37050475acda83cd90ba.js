47ed458401142c371a6c2408274b371c30f5f27b
Brian Whitaker <bwwhitaker@gmail.com>
2019-12-26 18:29:45 -0800

    Create build

---
+++ build/precache-manifest.c60cb5fa9d7b37050475acda83cd90ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d483ed3f09532c1e3c2d128d59c79e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbccf38999d45c343ef",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "1fb6ac362e6121e45700",
+    "url": "/static/css/main.150196a0.chunk.css"
+  },
+  {
+    "revision": "ccbccf38999d45c343ef",
+    "url": "/static/js/2.91dc7d06.chunk.js"
+  },
+  {
+    "revision": "1fb6ac362e6121e45700",
+    "url": "/static/js/main.64c64244.chunk.js"
+  },
+  {
+    "revision": "f3075bad7544d51b4397",
+    "url": "/static/js/runtime-main.e68a81cc.js"
+  }
+]);
